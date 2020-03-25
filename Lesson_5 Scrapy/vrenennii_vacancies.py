ac35eb778f5765414cae0c9827f5b15b56ed1de4
Andrey Koryagin <53011800+koryagin2006@users.noreply.github.com>
2020-03-25 21:20:21 +0500

    Update vrenennii_vacancies.py

--- Lesson_5 Scrapy/vrenennii_vacancies.py
+++ Lesson_5 Scrapy/vrenennii_vacancies.py
@@ -1,5 +1,6 @@
 import requests
 from lxml import html
+import json
 
 header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                         'Chrome/80.0.3987.149 Safari/537.36 '}
@@ -9,8 +10,13 @@ response = html.fromstring(response)
 
 vacancy_name = response.xpath("//h1[@class='bloko-header-1']/text()")[0]
 company = ''.join(response.xpath("//span[@class='bloko-section-header-2 bloko-section-header-2_lite']/text()"))
-salary = response.xpath("//div[@class='supernova-footer']/@data-params")
+vacancy_info_json = ''.join(response.xpath("//script[@data-name='HH/GoogleDfpService']/@data-params"))
+location = ''.join(response.xpath("//div[@class='vacancy-company vacancy-company_with-logo']//p/span[1]/text()"))
 
 
-for i in [vacancy_name, company, salary]:
+# salary_min = json.loads(vacancy_info_json)['vac_salary_from']
+# salary_max = json.loads(vacancy_info_json)['vac_salary_to']
+# currency = json.loads(vacancy_info_json)['vac_salary_cur']
+
+for i in [vacancy_name, company, location]:
     print(i, '\n')
