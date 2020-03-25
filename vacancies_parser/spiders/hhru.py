387d0da7959fc2e06d1f9c602e97af85b73b3873
Andrey Koryagin <53011800+koryagin2006@users.noreply.github.com>
2020-03-25 18:44:05 +0500

    Update hhru.py

--- vacancies_parser/spiders/hhru.py
+++ vacancies_parser/spiders/hhru.py
@@ -1,11 +1,25 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from scrapy.http import HtmlResponse
 
 
 class HhruSpider(scrapy.Spider):
     name = 'hhru'
     allowed_domains = ['hh.ru']
-    start_urls = ['http://hh.ru/']
+    start_urls = ['https://hh.ru/search/vacancy?area=1&st=searchVacancy&text=Python']
 
-    def parse(self, response):
-        pass
+    def parse(self, response: HtmlResponse):
+        next_page = response.xpath("//a[@class='bloko-button HH-Pager-Controls-Next HH-Pager-Control']").extract_first()
+        if next_page is None:
+            yield
+        yield response.follow(next_page, callback=self.parse)
+
+        vacancy_list = response.xpath("//a[@class='bloko-link HH-LinkModifier']/@href").extract()
+
+        for link in vacancy_list:
+            yield response.follow(link, callback=self.vacancy_parse)
+
+    def vacancy_parse(self, response: HtmlResponse):
+        name = response.xpath("//h1[@class='bloko-header-1']/text()").extract()
+        salary = response.xpath("//span[@class='bloko-header-2 bloko-header-2_lite']/text()").extract()
+        print(name, salary)
