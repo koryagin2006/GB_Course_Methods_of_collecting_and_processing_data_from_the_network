f3a46e838aca2f2790b74249c8d08f50f94df06f
Andrey Koryagin <53011800+koryagin2006@users.noreply.github.com>
2020-03-25 17:50:59 +0500

    Настройка settings

--- vacancies_parser/settings.py
+++ vacancies_parser/settings.py
@@ -16,10 +16,15 @@ NEWSPIDER_MODULE = 'vacancies_parser.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'vacancies_parser (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 ' \
+             'Safari/537.36 '
+
+LOG_ENABLED = True
+LOG_LEVEL = 'DEBUG'     # INFO, ERROR
+# LOG_FILE = 'logs.txt'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
@@ -33,7 +38,7 @@ ROBOTSTXT_OBEY = True
 #CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
-#COOKIES_ENABLED = False
+COOKIES_ENABLED = False
 
 # Disable Telnet Console (enabled by default)
 #TELNETCONSOLE_ENABLED = False
