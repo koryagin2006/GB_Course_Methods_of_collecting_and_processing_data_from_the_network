47cbd197cf74dfc2f4ab876ff7b9febeb58b87cb
Andrey Koryagin <53011800+koryagin2006@users.noreply.github.com>
2020-03-28 02:31:46 +0500

    Update pipelines.py

--- vacancies_parser/pipelines.py
+++ vacancies_parser/pipelines.py
@@ -4,8 +4,14 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-
+from pymongo import MongoClient
 
 class VacanciesParserPipeline(object):
+    def __init__(self):
+        client = MongoClient('localhost', 27017)
+        self.mongobase = client.vacansy_305
+
     def process_item(self, item, spider):
+        collection = self.mongobase[spider.name]
+
         return item
