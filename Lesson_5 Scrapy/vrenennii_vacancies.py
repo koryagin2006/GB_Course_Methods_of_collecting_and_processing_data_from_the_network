da94e1a4b97d33d918e60a6ad1ba373dbbf3509b
Andrey Koryagin <53011800+koryagin2006@users.noreply.github.com>
2020-03-28 12:48:14 +0500

    Update vrenennii_vacancies.py

--- Lesson_5 Scrapy/vrenennii_vacancies.py
+++ Lesson_5 Scrapy/vrenennii_vacancies.py
@@ -1,3 +1,4 @@
+from pprint import pprint
 import requests
 from lxml import html
 import json
@@ -5,18 +6,26 @@ import json
 header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                         'Chrome/80.0.3987.149 Safari/537.36 '}
 
-response = requests.get('https://hh.ru/vacancy/36396758?query=Python', headers=header).text
+response = requests.get('https://russia.superjob.ru/vakansii/sistemnyj-analitik-33619990.html', headers=header).text
+# response = requests.get('https://hh.ru/vacancy/36396758?query=Python', headers=header).text
 response = html.fromstring(response)
 
-vacancy_name = response.xpath("//h1[@class='bloko-header-1']/text()")[0]
-company = ''.join(response.xpath("//span[@class='bloko-section-header-2 bloko-section-header-2_lite']/text()"))
-vacancy_info_json = ''.join(response.xpath("//script[@data-name='HH/GoogleDfpService']/@data-params"))
-location = ''.join(response.xpath("//div[@class='vacancy-company vacancy-company_with-logo']//p/span[1]/text()"))
+# vacancy_list = response.xpath("//div[@class='_3zucV _2GPIV f-test-vacancy-item i6-sc _3VcZr']//div[@class='_2g1F-']/a/@href")
+# for i in vacancy_list:
+#     print(i)
+
+vacancy_name = response.xpath("//h1[@class='_3mfro rFbjy s1nFK _2JVkc']/text()")[0]
+company = ''.join(response.xpath("//h2[@class='_3mfro PlM3e _2JVkc _2VHxz _3LJqf _15msI']/text()"))
+vacancy_info_json = json.loads(response.xpath("//script[@type='application/ld+json']/text()")[1])
+location = ''.join(response.xpath("//span[@class='_6-z9f']//span[@class='_3mfro _1hP6a _2JVkc']/text()"))
 
 
-# salary_min = json.loads(vacancy_info_json)['vac_salary_from']
-# salary_max = json.loads(vacancy_info_json)['vac_salary_to']
-# currency = json.loads(vacancy_info_json)['vac_salary_cur']
+# salary_min = salary['value']['minValue']
+# salary_max = salary['value']['maxValue']
+# currency = salary['currency']
+#
+# for i in [salary_min, salary_max, currency]:
+#     print(i)
 
-for i in [vacancy_name, company, location]:
-    print(i, '\n')
+# for i in [vacancy_name, company, location]:
+#     print(i, '\n')
