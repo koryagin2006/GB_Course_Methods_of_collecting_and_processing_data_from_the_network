120e7a5e5d13be2d2811c2515b8140f17b26ae2b
Andrey Koryagin <53011800+koryagin2006@users.noreply.github.com>
2020-03-28 11:35:13 +0500

    Update runner.py

--- vacancies_parser/runner.py
+++ vacancies_parser/runner.py
@@ -5,11 +5,11 @@ from vacancies_parser.spiders.hhru import HhruSpider
 from vacancies_parser.spiders.sjru import SjruSpider
 
 if __name__ == '__main__':
+    search_query = 'Python'
+
     crawler_settings = Settings()
     crawler_settings.setmodule(settings)
     process = CrawlerProcess(settings=crawler_settings)
-
     process.crawl(HhruSpider)
     # process.crawl(SjruSpider)
-
     process.start()
