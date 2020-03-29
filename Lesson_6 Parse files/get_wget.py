57940191750138116fabacde7a155e282ef8a659
Andrey Koryagin <53011800+koryagin2006@users.noreply.github.com>
2020-03-29 14:48:10 +0500

    Update get_wget.py

--- Lesson_6 Parse files/get_wget.py
+++ Lesson_6 Parse files/get_wget.py
@@ -1,19 +1,11 @@
-###Загружаем с помощью requests
+# Загружаем с помощью requests
 import requests
-file = requests.get('https://klike.net/uploads/posts/2019-07/1562069947_1.jpg','sea.jpg')
-with open('sea.jpg','wb') as f:
+import wget
+
+file = requests.get('https://klike.net/uploads/posts/2019-07/1562069947_1.jpg', 'sea.jpg')
+with open('sea.jpg', 'wb') as f:
     f.write(file.content)
 
+# Загружаем с помощью wget
 
-###Загружаем с помощью wget
-import wget
-wget.download('https://klike.net/uploads/posts/2019-07/1562069947_1.jpg','sea.jpg')
-
-
-
-
-
-
-
-
-
+wget.download('https://klike.net/uploads/posts/2019-07/1562069947_1.jpg', 'sea.jpg')
