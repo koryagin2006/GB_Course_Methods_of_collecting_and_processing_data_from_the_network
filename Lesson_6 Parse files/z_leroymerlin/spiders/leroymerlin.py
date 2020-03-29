a31f9f55ed351ae885db7a7a8d40931fa9143bab
Andrey Koryagin <53011800+koryagin2006@users.noreply.github.com>
2020-03-29 14:34:14 +0500

    Создание паука

---
+++ Lesson_6 Parse files/z_leroymerlin/spiders/leroymerlin.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class LeroymerlinSpider(scrapy.Spider):
+    name = 'leroymerlin'
+    allowed_domains = ['leroymerlin.ru']
+    start_urls = ['http://leroymerlin.ru/']
+
+    def parse(self, response):
+        pass
