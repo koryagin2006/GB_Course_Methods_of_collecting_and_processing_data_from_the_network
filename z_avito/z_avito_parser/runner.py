898a01bd6bf60f6d86bd023932c6437a4a8625d5
Andrey Koryagin <53011800+koryagin2006@users.noreply.github.com>
2020-03-29 22:41:26 +0500

    Create runner.py

---
+++ z_avito/z_avito_parser/runner.py
@@ -0,0 +1,12 @@
+from scrapy.crawler import CrawlerProcess
+from scrapy.settings import Settings
+
+from z_avito.z_avito_parser import settings
+from z_avito.z_avito_parser.spiders.avito import AvitoSpider
+
+if __name__ == '__main__':
+    crawler_settings = Settings()
+    crawler_settings.setmodule(settings)
+    process = CrawlerProcess(settings=crawler_settings)
+    process.crawl(AvitoSpider)
+    process.start()
