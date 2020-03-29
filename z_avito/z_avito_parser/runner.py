3a00ffb1c90ae2a0825bbabf495f76a96a7a8026
Andrey Koryagin <53011800+koryagin2006@users.noreply.github.com>
2020-03-29 22:45:54 +0500

    Update runner.py

--- z_avito/z_avito_parser/runner.py
+++ z_avito/z_avito_parser/runner.py
@@ -8,5 +8,5 @@ if __name__ == '__main__':
     crawler_settings = Settings()
     crawler_settings.setmodule(settings)
     process = CrawlerProcess(settings=crawler_settings)
-    process.crawl(AvitoSpider)
+    process.crawl(AvitoSpider, search='диван')
     process.start()
