5b4ba7f1a98cf92745e161b9a230d22becda64ea
Andrey Koryagin <53011800+koryagin2006@users.noreply.github.com>
2020-03-30 21:56:34 +0500

    Update settings.py

--- _Scrapy_projects/avito/avito_parser/settings.py
+++ _Scrapy_projects/avito/avito_parser/settings.py
@@ -19,7 +19,9 @@ NEWSPIDER_MODULE = 'avito_parser.spiders'
 #USER_AGENT = 'avito_parser (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
+LOG_ENABLED = True
+LOG_LEVEL = 'DEBUG'
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
@@ -27,10 +29,10 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
-#CONCURRENT_REQUESTS_PER_DOMAIN = 16
-#CONCURRENT_REQUESTS_PER_IP = 16
+CONCURRENT_REQUESTS_PER_DOMAIN = 8
+CONCURRENT_REQUESTS_PER_IP = 8
 
 # Disable cookies (enabled by default)
 #COOKIES_ENABLED = False
