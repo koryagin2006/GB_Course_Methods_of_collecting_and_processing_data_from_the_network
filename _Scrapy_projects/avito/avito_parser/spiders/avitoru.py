23ce9c534687f064605044c4de93ecb1d19ef7ef
Andrey Koryagin <53011800+koryagin2006@users.noreply.github.com>
2020-03-31 17:14:48 +0500

    Update avitoru.py

--- _Scrapy_projects/avito/avito_parser/spiders/avitoru.py
+++ _Scrapy_projects/avito/avito_parser/spiders/avitoru.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 import scrapy
 from scrapy.http import HtmlResponse
+from avito_parser.items import AvitoParserItem
 
 
 class AvitoruSpider(scrapy.Spider):
     name = 'avitoru'
     allowed_domains = ['avito.ru']
-    start_urls = ['https://www.avito.ru/rossiya/kvartiry']
+    start_urls = ['']
 
-    def __init__(self, response: HtmlResponse):
+    def __init__(self, search):
         start_urls = [f'https://www.avito.ru/rossiya?q={search}']
 
     def parse(self, response):
@@ -19,5 +20,5 @@ class AvitoruSpider(scrapy.Spider):
     def parse_ads(self, response: HtmlResponse):
         photos = response.xpath("//div[@class='gallery-img-wrapper js-gallery-img-wrapper']/div[1]/@data-url").extract()
         name = response.xpath("//span[@class='title-info-title-text']/text()").extract_first()
-
-        print(name, photos)
+        # print(name, photos)
+        yield AvitoParserItem(name=name, photos=photos)
