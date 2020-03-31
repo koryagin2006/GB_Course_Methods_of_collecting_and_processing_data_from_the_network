85ffaee4a666ba78d6ab538c0bbde3cc80f63ae1
Andrey Koryagin <53011800+koryagin2006@users.noreply.github.com>
2020-04-01 01:09:05 +0500

    merge

--- _Scrapy_projects/avito/avito_parser/pipelines.py
+++ _Scrapy_projects/avito/avito_parser/pipelines.py
@@ -27,6 +27,7 @@ class AvitoPhotosPipeline(ImagesPipeline):
                 try:
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
                     yield scrapy.Request(img)
                 except Exception as e:
                     print(e)
@@ -35,6 +36,11 @@ class AvitoPhotosPipeline(ImagesPipeline):
         if results:
             item['photos'] = [itm[1] for itm in results if itm[0]]
         return item
+=======
+                    yield scrapy.Request(f'https:{img}')
+                except TypeError as e:
+                    print(e)
+>>>>>>> parent of d900147... Update pipelines.py
 =======
                     yield scrapy.Request(f'https:{img}')
                 except TypeError as e:
