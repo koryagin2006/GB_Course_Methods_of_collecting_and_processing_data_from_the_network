6024d64032f97c649405a90fe49276a83c24bfad
Andrey Koryagin <53011800+koryagin2006@users.noreply.github.com>
2020-03-31 17:58:52 +0500

    Обработка фотографий в pipline

--- _Scrapy_projects/avito/avito_parser/pipelines.py
+++ _Scrapy_projects/avito/avito_parser/pipelines.py
@@ -4,8 +4,20 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import scrapy
+from scrapy.pipelines.images import ImagesPipeline
 
 
 class AvitoParserPipeline(object):
     def process_item(self, item, spider):
         return item
+
+
+class AvitoPhotosPipeline(ImagesPipeline):
+    def get_media_requests(self, item, info):
+        if item['photos']:
+            for img in item['photos']:
+                try:
+                    yield scrapy.Request(f'https:{img}')
+                except TypeError as e:
+                    print(e)
