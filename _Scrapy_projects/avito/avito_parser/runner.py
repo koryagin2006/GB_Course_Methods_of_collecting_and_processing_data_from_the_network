32b08248f4ac10cb6fe611de1f695ac2fce33fe8
Andrey Koryagin <53011800+koryagin2006@users.noreply.github.com>
2020-04-02 17:07:58 +0500

    работает сбор ссылок с авито

--- _Scrapy_projects/avito/avito_parser/runner.py
+++ _Scrapy_projects/avito/avito_parser/runner.py
@@ -1,1 +1,14 @@
+from scrapy.crawler import CrawlerProcess
+from scrapy.settings import Settings
 
+from avito_parser.spiders.avito import AvitoSpider
+from avito_parser import settings
+
+if __name__ == '__main__':
+    crawler_settings = Settings()
+    crawler_settings.setmodule(settings)
+    process = CrawlerProcess(settings=crawler_settings)
+    # search_input = input('Please enter search: ')
+    search_input = 'квартира'
+    process.crawl(AvitoSpider, search=search_input)
+    process.start()
--- _Scrapy_projects/avito/avito_parser/spiders/avito.py
+++ _Scrapy_projects/avito/avito_parser/spiders/avito.py
@@ -1,11 +1,22 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from scrapy.http import HtmlResponse
 
 
 class AvitoSpider(scrapy.Spider):
     name = 'avito'
     allowed_domains = ['avito.ru']
-    start_urls = ['http://avito.ru/']
+    # start_urls = ['-']
 
-    def parse(self, response):
-        pass
+    def __init__(self, search):
+        self.start_urls = [f'https://www.avito.ru/moskva?q={search}']
+
+    def parse(self, response: HtmlResponse):
+        ads_links = response.xpath("//div[@class='snippet-title-row']//a/@href").extract()
+        for link in ads_links:
+            yield response.follow(link, callback=self.parse_ads)
+
+    def parse_ads(self, response: HtmlResponse):
+        photos = response.xpath("//div[@class='gallery-img-wrapper js-gallery-img-wrapper']/div/@data-url").extract()
+        name = response.xpath("//span[@class='title-info-title-text']/text()").extract_first()
+        print(name, photos)
