ffe133850c6c097c5f39af6fca3ef39ba097e7e0
Andrey Koryagin <53011800+koryagin2006@users.noreply.github.com>
2020-04-03 16:58:30 +0500

    ДЗ готово

--- _Scrapy_projects/leroymerlin/leroymerlin_parser/items.py
+++ _Scrapy_projects/leroymerlin/leroymerlin_parser/items.py
@@ -6,22 +6,15 @@
 # https://doc.scrapy.org/en/latest/topics/items.html
 
 import scrapy
-from scrapy.loader.processors import MapCompose, TakeFirst
-
-
-def cleaner_price(value):
-    value = value.replace(' ', '')
-    value = int(value)
-
-def cleaner_vendor_code(value):
-    value = int(value)
+from scrapy.loader.processors import MapCompose, TakeFirst, Join, Identity
 
 
 class LeroymerlinParserItem(scrapy.Item):
     # define the fields for your item here like:
     _id = scrapy.Field()
     name = scrapy.Field(output_processor=TakeFirst())
-    description = scrapy.Field(output_processor=TakeFirst())
+    description = scrapy.Field(output_processor=Join())
     photos = scrapy.Field()
-    vendor_code = scrapy.Field(input_processor=MapCompose(cleaner_vendor_code))
-    price = scrapy.Field(input_processor=MapCompose(cleaner_price))
+    image_path = scrapy.Field()
+    vendor_code = scrapy.Field()
+    price = scrapy.Field()
--- _Scrapy_projects/leroymerlin/leroymerlin_parser/pipelines.py
+++ _Scrapy_projects/leroymerlin/leroymerlin_parser/pipelines.py
@@ -23,4 +23,34 @@ class DataBasePipeline(object):
 
 class LeroymerlinParserPipeline(object):
     def process_item(self, item, spider):
+        item['price'] = int(item['price'][0].replace(' ', ''))
+        item['vendor_code'] = int(item['vendor_code'][0])
+        return item
+
+
+class LeroyPhotosPipeline(ImagesPipeline):
+    def get_media_requests(self, item, info):
+        if item['photos']:
+            for img in item['photos']:
+                try:
+                    yield scrapy.Request(img)
+                except TypeError as e:
+                    print(e)
+
+    def item_completed(self, results, item, info):
+        if results:
+            item['photos'] = [itm[1] for itm in results if itm[0]]
+        return item
+
+
+class MyImagesPipeline(ImagesPipeline):
+    def get_media_requests(self, item, info):
+        for image_url in item['photos']:
+            yield scrapy.Request(image_url)
+
+    def item_completed(self, results, item, info):
+        image_path = [x['path'] for ok, x in results if ok]
+        if not image_path:
+            raise DropItem("Item contains no images")
+        item['image_path'] = image_path
         return item
