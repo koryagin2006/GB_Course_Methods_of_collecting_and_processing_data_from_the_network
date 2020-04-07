77699f1e2771e7de2e006d7ea1fc43409b38ce19
Andrey Koryagin <53011800+koryagin2006@users.noreply.github.com>
2020-04-07 17:17:14 +0500

    Update pipelines.py

--- _Scrapy_projects/vacancies/vacancies_parser/pipelines.py
+++ _Scrapy_projects/vacancies/vacancies_parser/pipelines.py
@@ -6,6 +6,7 @@
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 from pymongo import MongoClient
 from vacancies_parser.runner import search_input
+import csv
 
 
 class VacanciesParserPipeline(object):
@@ -46,4 +47,28 @@ class VacanciesParserPipeline(object):
                 item['salary_max'] = int(item['vacancy_info_json']['vac_salary_to'])
 
         collection.insert_one(item)
+        return item
+
+
+class CSVPipeline():
+    def __init__(self):
+        self.file = open(f'database_{search_input}.csv', 'w', encoding='utf-8').close()
+
+        self.file = f'database_{search_input}.csv'
+        with open(self.file, 'r', newline='') as csv_file:
+            self.tmp_data = csv.DictReader(csv_file).fieldnames
+
+        self.csv_file = open(self.file, 'a', newline='', encoding='UTF-8')
+
+    def __del__(self):
+        self.csv_file.close()
+
+    def process_item(self, item, spider):
+        columns = item.fields.keys()
+
+        data = csv.DictWriter(self.csv_file, columns)
+        if not self.tmp_data:
+            data.writeheader()
+            self.tmp_data = True
+        data.writerow(item)
         return item
