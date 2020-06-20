570e5da384f5d2191f794ad0b790d16f5096ff17
Andrey Koryagin <53011800+koryagin2006@users.noreply.github.com>
2020-06-20 15:44:13 +0500

    Update settings.py

--- _Scrapy_projects/farmlend/farmlend_parser/settings.py
+++ _Scrapy_projects/farmlend/farmlend_parser/settings.py
@@ -21,6 +21,9 @@ USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTM
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
+LOG_ENABLED = True
+LOG_LEVEL = 'DEBUG'  # INFO, ERROR
+# LOG_FILE = 'logs.txt'
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
@@ -28,7 +31,7 @@ ROBOTSTXT_OBEY = False
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 3
+# DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
